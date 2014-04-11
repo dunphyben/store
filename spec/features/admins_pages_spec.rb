ff3d4fc5b5a0a1c383ea44a82b007f11c946abfc
Benjamin Paul Dunphy & Jason Mannon <student@epicodus.com>
2014-04-11 16:50:40 -0700

    Fix Capybara first test for Admin login authentication

--- spec/features/admins_pages_spec.rb
+++ spec/features/admins_pages_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe Admin do
     context "log in" do
         it 'lets an admin log in' do
-            admin = Admin.create
-            visit root_path
+            admin = FactoryGirl.create(:admin)
+            visit 'sessions/new'
             fill_in "Email", with: admin.email
             fill_in "Password", with: admin.password
             click_button "Log In"
             page.should have_content "Admin logged in successfully"
-            page.should have_content "Logged in as #{admin.name}"
         end
     end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,6 @@
 def create_admin_and_sign_in
   admin = create :admin
-  visit root_path
+  visit 'sessions/new'
   fill_in 'Email', with: admin.email
   fill_in 'Password', with: admin.password
   click_button 'Log In'
